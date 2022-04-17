ea329a5fe1bc6ce753b9afa08a6f1a10203ca7b0
Marvin <nopparut.bit@hotmail.com>
2022-04-17 22:32:26 +0700

    Add comment

--- src/main/java/LatencyWithGateway.java
+++ src/main/java/LatencyWithGateway.java
@@ -20,6 +20,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class LatencyWithGateway {
+    //ver11
     private static final Logger log = LoggerFactory.getLogger(LatencyWithGateway.class);
 
     public static void main(String[] args) throws Exception {
